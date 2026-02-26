8a152c03cf1f1f3d51dc057ddfb43cba7cb78953
Gabriel Barreto <barretogabriel972@gmail.com>
2026-02-26 10:05:09 -0300

    first class, Position

---
+++ Position.java
@@ -0,0 +1,43 @@
+public class Position
+{
+    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
+    private int row;
+    private int column;
+
+    
+    public Position(int row , int column)
+    {
+        this.row = row;
+        this.column = column;
+    
+    }
+
+   
+    public void setColumn(int column)
+    {
+        this.column = column;
+    }
+    
+    public int getColumn()
+    {
+        return column;
+    }
+    
+    public void setRow(int row)
+    {
+        this.row = row;
+    }
+    
+    public int getRow()
+    {
+        return row;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return row + ","+ column;
+    }
+
+    
+}
